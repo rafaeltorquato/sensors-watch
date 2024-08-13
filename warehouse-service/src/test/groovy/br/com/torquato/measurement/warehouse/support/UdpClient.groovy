9c66a5bf492efe7464fd7874432d590c45390017
Rafael Torquato <rafaeltorquat0@protonmail.com>
2024-08-13 19:55:42 -0300

    ♻️ Remove unnecessary semicolon.

--- warehouse-service/src/test/groovy/br/com/torquato/measurement/warehouse/support/UdpClient.groovy
+++ warehouse-service/src/test/groovy/br/com/torquato/measurement/warehouse/support/UdpClient.groovy
@@ -2,22 +2,22 @@ package br.com.torquato.measurement.warehouse.support
 
 class UdpClient {
 
-    private DatagramSocket socket;
-    private InetAddress address;
+    private DatagramSocket socket
+    private InetAddress address
     private final int port
 
     UdpClient(String host, int port) {
         this.port = port
-        socket = new DatagramSocket();
-        address = InetAddress.getByName(host);
+        socket = new DatagramSocket()
+        address = InetAddress.getByName(host)
     }
 
     def sendEcho(String msg) {
-        final byte[] buf = msg.getBytes();
-        socket.send(new DatagramPacket(buf, buf.length, address, port));
+        final byte[] buf = msg.getBytes()
+        socket.send(new DatagramPacket(buf, buf.length, address, port))
     }
 
     def close() {
-        socket.close();
+        socket.close()
     }
 }
